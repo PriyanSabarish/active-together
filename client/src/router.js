9d22050f194b559111d2888106087ad58458cea4
zhixin shao <zhixinshao23@gmail.com>
2026-08-29 14:29:50 +1000

    feat: add the time page with mock hourly forecast
    now / pick-a-time toggle, duration slider mapped to activity
    plans, and a weather card driven by the mock forecast table

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LocationView from './views/LocationView.vue'
+import TimeView from './views/TimeView.vue'
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'location', component: LocationView }
+    { path: '/', name: 'location', component: LocationView },
+    { path: '/time', name: 'time', component: TimeView }
   ]
 })
