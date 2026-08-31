5912e0423c543e1c5ba4ded4b1cc4de6da2e5dae
Ritvik Sharma <sharmaritvik393@gmail.com>
2026-09-01 01:12:09 +1000

    making the changes to fetch the data from database , changing the query pattern , also changing the classifcation confidencetype

--- server/app/data/database.py
+++ server/app/data/database.py
@@ -1,8 +1,14 @@
 from typing import Generator
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, Session
-
+from dotenv import load_dotenv
 from app.config import settings
+import logging
+import os
+load_dotenv()  # This explicitly forces python to read the .env file
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     settings.database_url,
@@ -12,7 +18,6 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
